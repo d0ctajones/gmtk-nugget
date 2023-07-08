7a8ef30e0f4cffe16ccb60b0b0cc5e49e7a4dd73
Dylan Jones <dylanjones@protonmail.com>
2023-07-08 02:17:42 -0700

    finally fix jitter on slopes. basic movement feeling okay

--- player/player.gd
+++ player/player.gd
@@ -1,16 +1,16 @@
 extends KinematicBody2D
 
-export (int) var max_ground_speed       = 200
-export (int) var ground_acceleration    = 50
-export (int) var ground_friction        = 25
-export (int) var gravity                = 12
-export (int) var max_fall_speed         = 250
+export (int) var max_ground_speed       = 250
+export (int) var ground_acceleration    = 30
+export (int) var ground_friction        = 50
+export (int) var gravity                = 2
+export (int) var max_fall_speed         = 700
 export (int) var jump_speed             = 350
-export (int) var air_move_speed         = 50
+export (int) var air_move_speed         = 120
 export (int) var air_acceleration       = 100
-export (int) var air_friction           = 50
-export (float) var vertical_jump_time   = 0.2
-export (float) var no_jump_yet_time     = 0.18
+export (int) var air_friction           = 20
+export (float) var vertical_jump_time   = 0.25
+export (float) var no_jump_time         = 0.18
 export (Vector2) var velocity           = Vector2.ZERO
 
 # Possible player states
@@ -20,29 +20,29 @@ enum STATES {
     FALL,
 }
 
-export (STATES) var state = STATES.MOVE
-
+var state = STATES.MOVE
 
 func _physics_process(delta):
-    if state != STATES.JUMP:
-        respect_gravity(delta)
-
     match state:
         STATES.MOVE: move_state(delta)
         STATES.JUMP: jump_state(delta)
         STATES.FALL: fall_state(delta)
 
+    move(delta)
+
+################################################################################
+# State Functions
+
 func move_state(delta):
+    var input_vector = Vector2.ZERO
+
     if Input.is_action_pressed("jump"):
-        if has_node("landing_timer"):
-            # Prevent the player from spamming jump too frequently based on no_jump_yet_time
+        if has_node("no_jump_timer"):
             pass
         else:
             state = STATES.JUMP
+            return
 
-    var input_vector = Vector2.ZERO
-
-    # Get left or right movement input
     if Input.is_action_pressed("move_right"):
         input_vector.x = 1
     if Input.is_action_pressed("move_left"):
@@ -58,24 +58,17 @@ func move_state(delta):
     # Limit to max groundspeed
     velocity.x = clamp(velocity.x, -max_ground_speed, max_ground_speed)
 
-    # Move and smootly go over collision
-    velocity = move_and_slide(velocity)
-
 func jump_state(delta):
     if has_node("jump_timer"):
-        # Only create one jump timer
-        pass
+        velocity.y = -jump_speed
     else:
-        # Set timer that allows player to ingore gravity until vertical_jump_time passes
-        var timer = Timer.new()
-        timer.name = "jump_timer"
-        timer.set_one_shot(true)
-        timer.set_wait_time(vertical_jump_time)
-        timer.connect('timeout', timer, 'queue_free')
-        timer.connect('timeout', self, '_end_jump')
+        add_jump_timer()
 
-        add_child(timer)
-        timer.start()
+    if Input.is_action_just_released("jump"):
+        # Shorter jump height if space bar released early
+        if has_node("jump_timer"):
+            get_node("jump_timer").queue_free()
+            state = STATES.FALL
 
     var input_vector = Vector2.ZERO
 
@@ -86,34 +79,15 @@ func jump_state(delta):
         input_vector.x = -1
 
     if input_vector != Vector2.ZERO:
-        # Steadily move from no move speed to max speed based on ground_acceleration. Use the ground speed
-        # variables to carry momentum until fall state
         velocity.x = lerp(velocity.x, input_vector.x * max_ground_speed, ground_acceleration * delta)
     else:
-        # Steadily apply air_friction until x-axis speed is 0
-        velocity.x = lerp(velocity.x, 0, air_friction * delta)
+        velocity.x = lerp(velocity.x, 0, ground_friction * delta)
 
-    # Negative speed go up
-    velocity.y = -jump_speed
+    velocity.x = clamp(velocity.x, -max_ground_speed, max_ground_speed)
 
-    # Detect collision from above the player in the y-axis. Set the state to the fall state
-    var collision = move_and_collide(velocity * delta)
-
-    if collision:
-        var collision_direction = collision.normal
-
-        if collision_direction.y >= 0:
-            # Player hit their head
-            velocity.y = 0
-            state = STATES.FALL
-
-
-func _end_jump():
-    # After the jump_timer ends, endter the fall state
-    state = STATES.FALL
 
 func fall_state(delta):
-    # Enable air control
+    # Manages x-axis air control. Gravity application is in process_gravity
     var input_vector = Vector2.ZERO
 
     if Input.is_action_pressed("move_right"):
@@ -121,62 +95,77 @@ func fall_state(delta):
     if Input.is_action_pressed("move_left"):
         input_vector.x = -1
 
-    # Move in the air according to air speed and ground_acceleration
     if input_vector != Vector2.ZERO:
         velocity.x = lerp(velocity.x, input_vector.x * air_move_speed, air_acceleration * delta)
     else:
         velocity.x = lerp(velocity.x, 0, air_friction * delta)
 
-    # Limit x velocity to air control
     velocity.x = clamp(velocity.x, -air_move_speed, air_move_speed)
 
-    # Detect collisions while falling. Only take action against a collision if it's in the y-axis
-    var collision = move_and_collide(velocity * delta)
+################################################################################
+# Gravity and Move
+
+func process_gravity(delta):
+    if state == STATES.JUMP or is_on_floor():
+        # Don't apply gravity if currently jumping or touching the floor
+        return
+
+    velocity.y = lerp(velocity.y, max_fall_speed, gravity * delta)
+    velocity.y = clamp(velocity.y, 0, max_fall_speed)
+
+
+func move(delta):
+    process_gravity(delta)
+
+    # Test for collisions
+    var collision = move_and_collide(velocity * delta, true, true, true)
 
     if collision:
-        var collision_direction = collision.normal
+        if state == STATES.FALL:
+            if collision.normal.y <= -0.75:
+                # Player touched the floor
+                add_no_jump_timer()
+                state = STATES.MOVE
+        elif state == STATES.JUMP:
+            if collision.normal.y >= 0.75:
+                # Player hit head on ceiling
+                state = STATES.FALL
 
-        if collision_direction.y != 0:
-            velocity.y = 0
+    velocity = move_and_slide(velocity, Vector2(0, -1), true, 2, 0.75, true)
 
-            # Character landed on something. Swap to move state
-            state = STATES.MOVE
-        else:
-            velocity = move_and_slide(velocity)
+################################################################################
+# Timer Functions
 
+func add_no_jump_timer():
+    # Prevent player from hitting jump for a moment after landing
+    var timer = Timer.new()
+    timer.name = "no_jump_time"
+    timer.set_one_shot(true)
+    timer.set_wait_time(vertical_jump_time)
+    timer.connect('timeout', timer, 'queue_free')
+
+    add_child(timer)
+    timer.start()
+
+func add_jump_timer():
+    # Set timer that allows player to ingore gravity until vertical_jump_time passes
+    var timer = Timer.new()
+    timer.name = "jump_timer"
+    timer.set_one_shot(true)
+    timer.set_wait_time(vertical_jump_time)
+    timer.connect('timeout', timer, 'queue_free')
+    timer.connect('timeout', self, '_end_jump')
+
+    add_child(timer)
+    timer.start()
+
+func _end_jump():
+    state = STATES.FALL
+
+################################################################################
+# Main Game Loop Functions
 
 func spawn(spawn_position):
     position = spawn_position
     velocity = Vector2.ZERO
 
-func respect_gravity(delta):
-    # Get to max fall speed by stepping up with gravity constant
-    velocity.y = lerp(velocity.y, max_fall_speed, gravity * delta)
-    # Limit fall speed to max fall speed
-    velocity.y = clamp(velocity.y, 0 , max_fall_speed)
-
-    # Move down and check for collisions
-    var go_down = velocity * delta
-    var collision = move_and_collide(go_down)
-
-    if collision:
-        # Get collision diections
-        var collision_direction = collision.normal
-
-        if collision_direction.y != 0:
-            # If the collision is on the y axis, stop moving in the y axis
-            velocity.y = 0
-
-            if state == STATES.FALL and not has_node("landing_timer"):
-                # If the player is falling and they don't having a landing timer, start a landing
-                # timer. This prevents them from spamming jump when they land
-                var timer = Timer.new()
-                timer.name = "landing_timer"
-                timer.set_one_shot(true)
-                timer.set_wait_time(no_jump_yet_time)
-                timer.connect('timeout', timer, 'queue_free')
-                add_child(timer)
-                timer.start()
-
-            # Character landed on something. Swap to move state
-            state = STATES.MOVE
