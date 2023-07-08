68b71678b0c2c89fd0aebe5e33379d3a8cce38a7
Dylan Jones <dylanjones@protonmail.com>
2023-07-07 20:46:43 -0700

    hopefully spacing is better now. go away tabs

--- main.gd
+++ main.gd
@@ -3,19 +3,19 @@ extends Node
 var player_node = null
 
 func _ready():
-	load_level()
-	spawn_player()
+    load_level()
+    spawn_player()
 
 func spawn_player():
-	var player = load("res://player/player.tscn")
-	player_node = player.instance()
-	var player_spawn = $Level.get_node("PlayerSpawn").position
+    var player = load("res://player/player.tscn")
+    player_node = player.instance()
+    var player_spawn = $Level.get_node("PlayerSpawn").position
 
-	player_node.spawn(player_spawn)
-	add_child(player_node)
+    player_node.spawn(player_spawn)
+    add_child(player_node)
 
 func load_level():
-	var level = load("res://level/level.tscn")
-	var level_instance = level.instance()
+    var level = load("res://level/level.tscn")
+    var level_instance = level.instance()
 
-	add_child(level_instance)
+    add_child(level_instance)
